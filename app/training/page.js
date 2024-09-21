a3cd60a5ff1b72d91ebb2e735b6503483a4f7614
AugustoIseppe <augusto.iseppe@gmail.com>
2024-09-21 11:09:23 -0300

    218. Protecting routes against unauthenticated access

--- app/training/page.js
+++ app/training/page.js
@@ -1,6 +1,19 @@
+import { redirect } from 'next/navigation';
+
+import { verifyAuth } from '@/lib/auth';
 import { getTrainings } from '@/lib/training';
 
 export default async function TrainingPage() {
+  //antes de qualquer coisa, eu devo usar a função verifyAuth() para verificar se o usuário está logado
+  //se não estiver logado, eu redireciono ele para a página de login
+  //se estiver logado, eu continuo com o código normalmente
+  const result = await verifyAuth();
+
+  if(!result.user){
+    return redirect('/');
+  }
+
+
   const trainingSessions = getTrainings();
 
   return (
